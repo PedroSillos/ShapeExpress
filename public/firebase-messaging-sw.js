3a5c4125e20702d881592c8d5db560d33982cb1b
PedroSillos <pedro.sillos@gmail.com>
2026-05-07 12:32:58 -0300

    segurança: rotaciona chaves de API expostas e atualiza configuração do Firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBCuNvJbd_iET7qods388w3z6QDzsxRmYo",
-  authDomain: "gen-lang-client-0904218608.firebaseapp.com",
-  projectId: "gen-lang-client-0904218608",
-  storageBucket: "gen-lang-client-0904218608.firebasestorage.app",
-  messagingSenderId: "186132941611",
-  appId: "1:186132941611:web:9d41d79d00df322499536e"
+  apiKey: "AIzaSyCidBEXaCtS28jgobAyA3_MLBothNbDy30",
+  authDomain: "shapeexpress.firebaseapp.com",
+  projectId: "shapeexpress",
+  storageBucket: "shapeexpress.firebasestorage.app",
+  messagingSenderId: "129919049822",
+  appId: "1:129919049822:web:94aa1f4143935d10cfd4cf"
 });
 
 const messaging = firebase.messaging();
